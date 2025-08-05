fa1496a8897c4c826c255eebe3d4706badee9223
dario609 <dario.melappioni@gmail.com>
2025-08-05 11:32:08 +0200

    test2

--- index.test.js
+++ index.test.js
@@ -5,6 +5,6 @@ describe('GET /', () => {
   it('should return Hello CI/CD!', async () => {
     const res = await request(app).get('/');
     expect(res.statusCode).toBe(200);
-    expect(res.text).toBe('Hello CI/CD!');
+    expect(res.text).toBe('should return Hello CI/CD!');
   });
 });
