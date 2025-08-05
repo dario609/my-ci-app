83d48085fcf9deb445412a5f48f13d743407848e
dario609 <dario.melappioni@gmail.com>
2025-08-05 11:25:31 +0200

    check

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const express = require('express');
 const app = express();
-app.get('/', (req, res) => res.send('Hello CI/CD!'));
+app.get('/', (req, res) => res.send('Hello CI/CD This is for testing!'));
 module.exports = app;
