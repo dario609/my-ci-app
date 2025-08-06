5dda31a9513f5c1226adef6795ef94c14e807603
dario609 <dario.melappioni@gmail.com>
2025-08-06 20:09:00 +0200

    check again

--- index.test.js
+++ index.test.js
@@ -5,6 +5,6 @@ describe('GET /', () => {
   it('should return Hello CI/CD again!', async () => {
     const res = await request(app).get('/');
     expect(res.statusCode).toBe(200);
-    expect(res.text).toBe('Hello CI/CD This is for testing again again!');
+    expect(res.text).toBe('Hello CI/CD This is for testing again agai!');
   });
 });
