90dcd0fdc0a75944f101b4340a6d9e100ad649d6
dario609 <dario.melappioni@gmail.com>
2025-08-07 13:19:38 +0200

    PM2

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const express = require('express');
 const app = express();
-app.get('/', (req, res) => res.send('Hello, World!'));
+app.get('/', (req, res) => res.send('Hello, PM2!'));
 module.exports = app;
