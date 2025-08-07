114c0a15489eb4cbfb04fad1074f3bd0ca8ab84d
dario609 <dario.melappioni@gmail.com>
2025-08-07 11:59:21 +0200

    test ci/cd

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const express = require('express');
 const app = express();
-app.get('/', (req, res) => res.send('Hello!'));
+app.get('/', (req, res) => res.send('Hello, World!'));
 module.exports = app;
